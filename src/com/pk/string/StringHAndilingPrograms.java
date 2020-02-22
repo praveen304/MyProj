41505973a5615027ab52c8edbafca504c26a6630
praveen <praveen@PBiswas>
2020-02-22 15:34:28 +0530

    string sample

---
+++ src/com/pk/string/StringHAndilingPrograms.java
@@ -0,0 +1,11 @@
+package com.pk.string;
+
+public class StringHAndilingPrograms {
+
+	public static void main(String[] args) {
+		String s1="hari";
+		String s2=s1+"null".hashCode();
+		System.out.println(s2);
+	}
+}
+
